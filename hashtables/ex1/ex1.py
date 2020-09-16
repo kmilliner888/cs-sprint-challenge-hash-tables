d814a2c16756bdf168f93c541a1d099eca6c70e8
Kaylen Milliner <kaylen.milliner@gmail.com>
2020-09-16 00:32:32 -0400

    mvp

--- hashtables/ex1/ex1.py
+++ hashtables/ex1/ex1.py
@@ -1,8 +1,31 @@
+#Step 1: create a function
+#Step 2: find two items whose sum of weights equals the weight limit `limit`
+#Step 2.1: Create a hash table
+#Step 2.2: Store the weights as keys, and indexes as values
+#Step 2.3: Check if hash table has limit weight
+#Step 3: return a tuple of integers that has the following form: (zero, one)
+#Step 4: if we find nothing, return None
+
+#Constraint: Needs to run in Linear Time
+
+# input: weights = [ 4, 6, 10, 15, 16 ], length = 5, limit = 21
+# output: [ 3, 1 ]  # since these are the indices of weights 15 and 6 whose sum equals 21
+
+
 def get_indices_of_item_weights(weights, length, limit):
-    # two items whose sum of weights equals the weight limit
-    # returns a tuple of integers  (zero, one) where each represents the item weights of the two packages
-    # higher one goes in zeroth place
-    
+    dictionary = {}
 
+    for i in range (len(weights)):
+       dictionary[weights[i]] = i
 
+    for i in range (len(weights)):
+        weight_limit = limit - weights[i]
+
+        if weight_limit in dictionary:
+            return ([max(i, dictionary[weight_limit]), min(i,dictionary[weight_limit])])
     return None
+
+
+
+
+
--- hashtables/ex2/ex2.py
+++ hashtables/ex2/ex2.py
@@ -1,14 +1,33 @@
 #  Hint:  You may not need all of these.  Remove the unused functions.
+
+#Step 1: Create a hash table
+#Step 1.1: Create empty route
+#Step 2: Store source as key, destination as value
+#Step 3: Loop over every position in the table, starting from None
+#Step 4:
+
+
 class Ticket:
     def __init__(self, source, destination):
         self.source = source
         self.destination = destination
 
 
+
 def reconstruct_trip(tickets, length):
-    """
-    YOUR CODE HERE
-    """
-    # Your code here
+    dictionary = {}
+    route = []
+
+    for ticket in tickets:
+        dictionary[ticket.source] = ticket.destination
+
+    starting_point = dictionary["NONE"]
+
+    while starting_point is not "NONE":
+        route.append(starting_point)
+        starting_point = dictionary[starting_point]
+
+    route.append("NONE")
+
 
     return route
